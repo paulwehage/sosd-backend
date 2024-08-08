0663c36e5682a6e9ffff6aa5a2aee5f70e792f68
paulwehage <paulomatik@gmail.com>
2024-08-08 18:06:00 +0200

    allow cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // Die URL Ihres Frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Software Sustainability Dashboard API')
